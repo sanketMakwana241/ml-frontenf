6c4401ee8a8499c93291b867f672e3136a8b6b6c
sanketMakwana241 <22010101105@darshan.ac.in>
2025-03-13 14:01:42 +0530

    URL Change

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -55,7 +55,7 @@ export default function HomePage() {
     };
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("https://ml-project-4-4em5.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
